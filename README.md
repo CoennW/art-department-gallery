434eca44787b4bb3e5d0ab654a94101a1b395e04
Unknown <coen_walter@hotmail.com>
2019-04-25 13:01:26 +0200

    file creations

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # art-department-gallery
 Post art as visitor and show them to others. Get connected with other visitors that you like. 
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Upload your art</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+
+
+</head>
+
+<body>
+
+
+    <html>
+
+    <body>
+
+        <form action="upload.php" method="post">
+            Name: <input type="text" name="name"><br> 
+            E-mail: <input type="text" name="email"><br> 
+            Your art: <input type="file" name="art">
+            <input type="submit">
+        </form>
+
+    </body>
+
+    </html>
+
+
+</body>
+
+</html>
