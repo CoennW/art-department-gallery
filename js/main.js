e3efb5e422ca9985d0c9fafa21e9395117eee97a
Unknown <coen_walter@hotmail.com>
2019-04-27 14:51:20 +0200

    php and javascript ajax connection fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
 function validate(id, value) {
-    
+    console.log(value);
     var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
@@ -11,6 +11,4 @@ function validate(id, value) {
         xmlhttp.open("GET", "php/validate_delete.php?q=" + id, true);
         xmlhttp.send();
 }
-
-
 
--- validate_uploads.php
+++ validate_uploads.php
@@ -2,84 +2,115 @@
 
 include 'db_connect.php';
 
+
+
 $sql = "SELECT id, artist, name_artwork, instagram, path FROM uploaded_art";
+
 $result = mysqli_query($conn, $sql);
-
-
-
 
 ?>
 
 
-    <!doctype html>
 
-    <html lang="en">
+<!DOCTYPE html>
 
-    <head>
-        <meta charset="utf-8">
-
-        <title>Upload your art</title>
-        <meta name="description" content="THEARTDEPARTMENT">
-        <meta name="author" content="SitePoint">
-        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-        
-        
+<html lang="en">
 
 
 
-    </head>
+<head>
 
-    <body>
+    <meta charset="utf-8">
+
+    <title>Upload your art</title>
+
+    <meta name="description" content="THEARTDEPARTMENT">
+
+    <meta name="author" content="SitePoint">
+
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+
+</head>
 
 
-        <html>
 
-        <body>
-                <p>ID: <span id="txtHint"></span></p>
-            <?php
-                
-            
-            
-            
-            
-                if (mysqli_num_rows($result) > 0) {
-                    
-                    echo "<table border='1'> <tr>
-                            <th>ID</th>
-                            <th>Artist</th>
-                            <th>Name Artwork</th>
-                            <th>Art piece</th>
-                            <th>Yes/No</th>
-                            </tr>"; 
-                    
-                        // output data of each row
-                        while($row = mysqli_fetch_assoc($result)) {
-                            
-                            
-                        echo "<tr><td>".$row["id"]."</td>
-                        <td>".$row["artist"]."</td>
-                        <td>".$row["name_artwork"]."</td>
-                        <td><img src=".$row["path"]." height='30%'></td>
-                        <td><input type='submit' id=".$row["id"]." class='button' name='ok' value='ok' onclick='validate(".$row["id"].")'/>
-                        
-                        
-                        
-                        <input type='submit' id=".$row["id"]." class='button' name='delete' value='delete' onclick='validate(".$row["id"], ")'/></td></tr>";
-                            
-                        
-                            
-                            
-                            }
-                        } else {
-                        echo "0 results";
-                        }
-            
-            ?>
-                
-        </body>
-            
-        </html>
-<script src="js/main.js"></script>
-    </body>
+<body>
 
-    </html>
+  <p>ID: <span id="txtHint"></span></p>
+
+
+
+  <?php
+
+  $value = 'delete';
+
+
+
+  if (mysqli_num_rows($result) > 0)
+
+  {
+
+    echo '<table border="1"> <tr>
+
+            <th>ID</th>
+
+            <th>Artist</th>
+
+            <th>Name Artwork</th>
+
+            <th>Art piece</th>
+
+            <th>Yes/No</th>
+
+            </tr>';
+
+
+
+        // output data of each row
+
+        while($row = mysqli_fetch_assoc($result))
+
+        {
+
+          echo
+
+          '<tr><td>' . $row['id'] . '</td>
+
+          <td>' . $row['artist'] . '</td>
+
+          <td>' . $row['name_artwork'] . '</td>
+
+          <td><img src="' . $row['path'] . '" height="30%"></td>
+
+          <td><input type="submit" id="' . $row['id'] . '" class="button" name="ok" value="ok" onclick="validate(' . $row['id'] . ')">';
+
+
+
+          echo
+
+          '<input type="submit" id="' . $row['id'] . '" class="button" name="delete" value="delete" onclick="validate(' . $row['id'] . ',\'delete\')"></td></tr>';
+
+        }
+
+    }
+
+    else
+
+    {
+
+      echo '0 results';
+
+    }
+
+    ?>
+
+  <script src="js/main.js">
+
+
+
+  </script>
+
+</body>
+
+</html>
+
